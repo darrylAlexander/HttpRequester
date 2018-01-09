81ecb4f74c6823c5481c7c27c650dddf5fd9b480
darrylAlexander <mr.darryl.alexander@gmail.com>
2018-01-09 16:47:54 +0000

    Edit: basic get request using extension library and form to hard coded destination

---
+++ src/httprequester.js
@@ -0,0 +1,17 @@
+var HTTPREQUESTER = (function () {
+
+    function initialise() {
+
+        document.getElementById("httprequester_getRequest").onsubmit = function(){
+            try{
+                AJAXSubmit(this);
+            } catch {
+                console.log("There is a problem with the dependency AJAXSUBMIT.");
+            }
+
+            return false;
+        };
+    }
+
+    initialise();
+})();
--- src/popupInit.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    document.getElementById("getRequest").onsubmit = function(){
-        AJAXSubmit(this);
-        return false;
-    };
-};
